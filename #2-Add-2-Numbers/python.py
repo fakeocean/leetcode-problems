2926b98f8b6eb8f9dc82ff582f656dbb0d341b91
Maciej <timing_larks85@icloud.com>
2025-08-07 21:18:32 +0200

    a

--- #2-Add-2-Numbers/python.py
+++ #2-Add-2-Numbers/python.py
@@ -6,46 +6,8 @@ class ListNode:
         self.val = val
         self.next = next
 
-
 class Solution:
-    def getNumberArray(self, l: Optional[ListNode]):
-        current = l
-        n = []
-        while current:
-            n.append(current.val)
-            current = current.next
-        return n
-    
-    def arrayToNumber(self, array):
-        pow = 1
-        num = 0
-        for n in array:
-            num += (n * pow)
-            pow *= 10
-        return num
-
-    def convToListNode(self, number):
-        a = []
-        n = number
-        while n > 0:
-            a.append(n - (n // 10) * 10)
-            n //= 10
-        return a
-        
-            
-
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        a1 = Solution.getNumberArray(self, l1)
-        a2 = Solution.getNumberArray(self, l2)
-
-        n1 = Solution.arrayToNumber(self, a1)
-        n2 = Solution.arrayToNumber(self, a2)
-    
-        total = Solution.convToListNode(self, n1+n2)
-        head = None
-        for val in total:
-            head = ListNode(val, head)
-        return head
         
         
 
