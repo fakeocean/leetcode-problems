eda9e8f9072cfb63504f159d9921979ed3a4b6d9
Maciej <timing_larks85@icloud.com>
2025-08-07 09:26:37 +0200

    z

--- #2-Add-2-Numbers/python.py
+++ #2-Add-2-Numbers/python.py
@@ -6,10 +6,52 @@ class ListNode:
         self.val = val
         self.next = next
 
+
 class Solution:
+    def getNumberArray(self, l: Optional[ListNode]):
+        current = l
+        n = []
+        while current:
+            n.append(current.val)
+            current = current.next
+        return n
+    
+    def arrayToNumber(self, array):
+        pow = 1
+        num = 0
+        for n in array:
+            num += (n * pow)
+            pow *= 10
+        return num
+
+    def convToListNode(self, number):
+        a = []
+        n = number
+        while n > 0:
+            a.append(n - (n // 10) * 10)
+            n //= 10
+        return a
+        
+            
+
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        print(l1)
+        a1 = Solution.getNumberArray(self, l1)
+        a2 = Solution.getNumberArray(self, l2)
+
+        n1 = Solution.arrayToNumber(self, a1)
+        n2 = Solution.arrayToNumber(self, a2)
+    
+        total = Solution.convToListNode(self, n1+n2)
+        head = None
+        for val in total:
+            head = ListNode(val, head)
+        return head
+        
+        
+
 
 
 l1 = ListNode(2, ListNode(4, ListNode(3)))
-print(l1)
+l2 = ListNode(5, ListNode(6, ListNode(4)))
+s = Solution()
+print(s.addTwoNumbers(l1, l2))
--- #28-Needle-Stack/typescript.ts
+++ #28-Needle-Stack/typescript.ts
@@ -1,16 +1,14 @@
 function strStr(haystack: string, needle: string): number {
     for (let i = 0; i < haystack.length; i++) {
         if (haystack[i] === needle[0]) {
-            const index = i
             for (let j = 1; j < needle.length; i++) {
                 if (i+j < haystack.length) {
                     if (haystack[i+j] !== needle[j+i]) {
                         return -1
                     }
-                    return index
+                    return i
                 }
                 else return -1
-                
             }
         }
     }
